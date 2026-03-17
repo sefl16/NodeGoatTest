dfbe4c5b048c9c02f702f78289bc2a719edbe941
sefl16 <sefl16@student.bth.se>
2026-03-17 16:17:29 +0100

    Update research.js

--- app/routes/research.js
+++ app/routes/research.js
@@ -11,21 +11,6 @@ function ResearchHandler(db) {
 
     this.displayResearch = (req, res) => {
 
-        if (req.query.symbol) {
-            const url = req.query.url + req.query.symbol;
-            return needle.get(url, (error, newResponse, body) => {
-                if (!error && newResponse.statusCode === 200) {
-                    res.writeHead(200, {
-                        "Content-Type": "text/html"
-                    });
-                }
-                res.write("<h1>The following is the stock information you requested.</h1>\n\n");
-                res.write("\n\n");
-                if (body) {
-                    res.write(body);
-                }
-                return res.end();
-            });
         }
 
         return res.render("research", {
